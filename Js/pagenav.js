399ed3655ce0d4523e9e32292d714a5d77db0c48
mzz <1757705354@qq.com>
2016-06-01 21:18:25 +0800

    modify

--- Js/pagenav.js
+++ Js/pagenav.js
@@ -5,23 +5,24 @@ function myPageNation(size=20){
     var page_index,
         page_total;
     var pagesize=size;
+    function getpageindex(){
+        return page_index;
+    }
     function nextpage(){
-        if(page_index<page_total&&page_index>=0){
+        if(page_index+1<page_total&&page_index>0){
             page_index+=1;
+            paginationinit(page_total,page_index);
         }else{
             page_index=page_total;
         }
-        paginationinit(page_total,$('.pagination'),page_index);        
     }
     function prepage(){
         if(page_index<=page_total&&(page_index-1)>0){
             page_index-=1;
-            console.log(page_index);
-                // this.getallstrategy(this.page,this.pagesize);/
+            paginationinit(page_total,page_index);
         }else{
             page_index=1;
         }
-        paginationinit(page_total,$('.pagination'),page_index);
     }
     function paginationievent(e){
         var tagname=$(e.target);
@@ -33,17 +34,22 @@ function myPageNation(size=20){
             return;
         }else{
             page_index=parseInt(tagname.text().trim());
-            paginationinit(page_total,$('.pagination'),page_index);
+            paginationinit(page_total,page_index);
         }
+        console.log(getpageindex());
     }
-    function paginationinit(total,item,page){
+    // function produceli(i) {
+    //     var _li='<li class="active"><a href="javascript:void(0);">'+i+'</a></li>';
+    //     return _li;
+    // }
+    function paginationinit(total,page){
         page_total=total;
         page_index=page;
-        var paginationclass=item;
-        var contentpage='<li><a href="javascript:void(0);" v-on:click="prepage" class="pre">&laquo;</a></li>';
+        var paginationclass=$('.pagination');
+        var contentpage='<li><a href="javascript:void(0);" class="pre">&laquo;</a></li>';
         if (total==0) {
             contentpage+='<li><a href="javascript:void(0);">当前无任何记录</a></li>';
-        }else if(total<6){
+        }else if(total<5){
             for (var i=1;i<=total;i++) {
                 if(page==i){
                     contentpage+='<li class="active"><a href="javascript:void(0);">'+i+'</a></li>';
@@ -53,7 +59,7 @@ function myPageNation(size=20){
                 }
             }
         }else{
-            if(page<6){
+            if(page<4){
                 for (var i=1;i<=5;i++) {
                     if(page==i){
                         contentpage+='<li class="active"><a href="javascript:void(0);">'+i+'</a></li>';
@@ -64,19 +70,28 @@ function myPageNation(size=20){
                 }
                 contentpage+='<li><a href="javascript:void(0);">...</a></li><li><a href="javascript:void(0);">'+total+'</a></li>';
             }else{
-                if(page==6){
+                if(page==4){
                     contentpage+='<li><a href="javascript:void(0);">1</a></li>';
                 }else{
                     contentpage+='<li><a href="javascript:void(0);">1</a></li><li><a href="javascript:void(0);">...</a></li>';
                 }
-                for (var i=1,j=page_index-5;i<4;i++) {
-                    contentpage+='<li><a href="javascript:void(0);">'+(i+j)+'</a></li>';
-                }
-                contentpage+='<li class="active"><a href="javascript:void(0);">'+(page_index-1)+'</a></li>';
-                if(page_index==page_total){
-                    contentpage+='<li><a href="javascript:void(0);">'+total+'</a></li>';
-                }else{
+                if((page_index+2)<page_total){
+                    for (var i=1,j=page_index-3;i<6;i++) {
+                        if((i+j)==page_index){
+                            contentpage+='<li class="active"><a href="javascript:void(0);">'+page_index+'</a></li>';
+                        }else{
+                            contentpage+='<li><a href="javascript:void(0);">'+(i+j)+'</a></li>';
+                        }
+                    }
                     contentpage+='<li><a href="javascript:void(0);">...</a></li><li><a href="javascript:void(0);">'+total+'</a></li>';
+                }else{
+                    for (var i=1,j=page_index-3;i<(4+page_total-page_index);i++) {
+                        if((i+j)==page_index){
+                            contentpage+='<li class="active"><a href="javascript:void(0);">'+page_index+'</a></li>';
+                        }else{
+                            contentpage+='<li><a href="javascript:void(0);">'+(i+j)+'</a></li>';
+                        }
+                    }
                 }
             }
         }
@@ -84,8 +99,9 @@ function myPageNation(size=20){
         paginationclass.html(contentpage);
     }
     return {
-        "paginationinit":paginationinit,
-        "paginationievent":paginationievent
+        "pageInit":paginationinit,
+        "pageEvent":paginationievent,
+        "getPageIndex":getpageindex
     }
 }
     
