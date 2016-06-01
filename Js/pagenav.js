89e761e0c05e67fe9c9ec17b8d8a73da33060716
mzz <1757705354@qq.com>
2016-06-01 23:25:26 +0800

    complete jpnavtion

--- Js/pagenav.js
+++ Js/pagenav.js
@@ -1,17 +1,18 @@
 jQuery.extend({
     myPageNation:myPageNation
 });
-function myPageNation(size=20){
+function myPageNation(size=10){
     var page_index,
         page_total;
     var pagesize=size;
+    var _first=true;
     function getpageindex(){
         return page_index;
     }
     function nextpage(){
-        if(page_index+1<page_total&&page_index>0){
+        if(page_index<page_total&&page_index>0){
             page_index+=1;
-            paginationinit(page_total,page_index);
+            paginationinit(page_index);
         }else{
             page_index=page_total;
         }
@@ -19,7 +20,7 @@ function myPageNation(size=20){
     function prepage(){
         if(page_index<=page_total&&(page_index-1)>0){
             page_index-=1;
-            paginationinit(page_total,page_index);
+            paginationinit(page_index);
         }else{
             page_index=1;
         }
@@ -34,73 +35,74 @@ function myPageNation(size=20){
             return;
         }else{
             page_index=parseInt(tagname.text().trim());
-            paginationinit(page_total,page_index);
+            paginationinit(page_index);
         }
         console.log(getpageindex());
     }
-    // function produceli(i) {
-    //     var _li='<li class="active"><a href="javascript:void(0);">'+i+'</a></li>';
-    //     return _li;
-    // }
-    function paginationinit(total,page){
-        page_total=total;
+    function computepagetotal(total){
+        var _totalpage=total%pagesize;
+        if(_totalpage==0){
+            page_total=total/pagesize;
+        }else{
+            page_total=Math.floor(total/pagesize)+1;
+        }
+    }
+    function produceli(i) {
+        var _li='<li><a href="javascript:void(0);">'+i+'</a></li>';
+        return _li;
+    }
+    function produceliactive(i) {
+        var _li='<li class="active"><a href="javascript:void(0);">'+i+'</a></li>';
+        return _li;
+    }
+    function whichli(page,i){
+        if(page==i){
+            return produceliactive(i);
+        }else{
+            return produceli(i);
+        }
+    }
+    function paginationinit(page,total){
         page_index=page;
+        if(_first){
+            computepagetotal(total);
+            $('.pagination').on('click',paginationievent);
+            _first=false;
+        }
         var paginationclass=$('.pagination');
         var contentpage='<li><a href="javascript:void(0);" class="pre">&laquo;</a></li>';
-        if (total==0) {
-            contentpage+='<li><a href="javascript:void(0);">当前无任何记录</a></li>';
-        }else if(total<5){
-            for (var i=1;i<=total;i++) {
-                if(page==i){
-                    contentpage+='<li class="active"><a href="javascript:void(0);">'+i+'</a></li>';
-                }
-                else{
-                    contentpage+='<li><a href="javascript:void(0);">'+i+'</a></li>';
-                }
+        if (page_total==0) {
+            contentpage+=produceli("当前无任何记录");
+        }else if(page_total<8){
+            for (var i=1;i<=page_total;i++) {
+                contentpage+=whichli(page_index,i);
+            }
+        }else if(page<6){
+            for (var i=1;i<6;i++) {
+                contentpage+=whichli(page_index,i);
+            }
+            contentpage+=produceli("...");
+            contentpage+=produceli(page_total);
+        }else if(page_total-page_index<5){
+            contentpage+=produceli(1);
+            contentpage+=produceli("...");
+            for(var i=page_total-4;i<=page_total;i++){
+                contentpage+=whichli(page_index,i);
             }
         }else{
-            if(page<4){
-                for (var i=1;i<=5;i++) {
-                    if(page==i){
-                        contentpage+='<li class="active"><a href="javascript:void(0);">'+i+'</a></li>';
-                    }
-                    else{
-                        contentpage+='<li><a href="javascript:void(0);">'+i+'</a></li>';
-                    }
-                }
-                contentpage+='<li><a href="javascript:void(0);">...</a></li><li><a href="javascript:void(0);">'+total+'</a></li>';
-            }else{
-                if(page==4){
-                    contentpage+='<li><a href="javascript:void(0);">1</a></li>';
-                }else{
-                    contentpage+='<li><a href="javascript:void(0);">1</a></li><li><a href="javascript:void(0);">...</a></li>';
-                }
-                if((page_index+2)<page_total){
-                    for (var i=1,j=page_index-3;i<6;i++) {
-                        if((i+j)==page_index){
-                            contentpage+='<li class="active"><a href="javascript:void(0);">'+page_index+'</a></li>';
-                        }else{
-                            contentpage+='<li><a href="javascript:void(0);">'+(i+j)+'</a></li>';
-                        }
-                    }
-                    contentpage+='<li><a href="javascript:void(0);">...</a></li><li><a href="javascript:void(0);">'+total+'</a></li>';
-                }else{
-                    for (var i=1,j=page_index-3;i<(4+page_total-page_index);i++) {
-                        if((i+j)==page_index){
-                            contentpage+='<li class="active"><a href="javascript:void(0);">'+page_index+'</a></li>';
-                        }else{
-                            contentpage+='<li><a href="javascript:void(0);">'+(i+j)+'</a></li>';
-                        }
-                    }
-                }
-            }
+            contentpage+=produceli(1);
+            contentpage+=produceli("...");
+            contentpage+=produceli(page_index-1);
+            contentpage+=produceliactive(page_index);
+            contentpage+=produceli(page_index+1);
+            contentpage+=produceli("...");
+            contentpage+=produceli(page_total);
         }
         contentpage+='<li><a href="javascript:void(0);" class="next">&raquo;</a></li>';
         paginationclass.html(contentpage);
     }
     return {
         "pageInit":paginationinit,
-        "pageEvent":paginationievent,
         "getPageIndex":getpageindex
     }
 }
